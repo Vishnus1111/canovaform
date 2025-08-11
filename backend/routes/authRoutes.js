822c33e7b660005454e18be4cde38172ccc0277d
vishnu s <138383752+Vishnus1111@users.noreply.github.com>
2025-08-11 12:23:46 +0530

    U authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -76,6 +76,7 @@ router.post("/register", async (req, res) => {
   try {
     const existingUser = await User.findOne({ email });
     console.log("🔍 Existing User in DB:", existingUser); // Log if user exists
+    if (existingUser) return res.status(400).json({ error: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
